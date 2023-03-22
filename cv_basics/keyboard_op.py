3d689205f266b29b5bdeb062ec9f4e88fd1476f8
sendoru <sendol39@gmail.com>
2023-03-22 17:26:48 +0900

    fixed keyboard_op

--- cv_basics/keyboard_op.py
+++ cv_basics/keyboard_op.py
@@ -34,6 +34,7 @@ log_msg = \
 T: toggle trajectory recording
 E: erase all trajectory
 Q: kill subscriber node
+ESC: kill keyboard_op node
 '''
 
 def main():
@@ -44,22 +45,30 @@ def main():
     rclpy.init()
     qos = QoSProfile(depth=10)
     node = rclpy.create_node('keyboard_op')
+    cur_iter = 0
+
     try:
-        node.get_logger().log(log_msg)
         pub = node.create_publisher(std_msgs.msg.String, 'key_pressed', qos)
         node.prev_keys = deque(maxlen=3)
 
         while True:
+            if cur_iter % 100 == 0:
+                node.get_logger().info(log_msg)
             key = get_key(settings)
             if (not key in node.prev_keys) and (key in ('t', 'e', 'q')):
                 msg = std_msgs.msg.String()
                 msg.data = key
                 pub.publish(msg)
+                node.get_logger().info(key)
+            elif key != '' and (ord(key) == 27 or ord(key) == 3):
+                break
+
             node.prev_keys.append(key)
-            
+            cur_iter += 1
     finally:
         return
 
-
+if __name__ == "__main__":
+    main()
 
 
